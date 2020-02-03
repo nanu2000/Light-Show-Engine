11b0aa8cb29d8ed2b34338add05fb4148537b10f
Richard sikra <richiesikra@gmail.com>
2020-02-02 19:20:55 -0800

    Added index content

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/*! \mainpage Index
+ *Git repository and build instructions are located [here](https://github.com/nanu2000/Light-Show-Boilerplate).
+ *If you have any comments or questions please feel free to contact me @ richiesikra@gmail.com.
+ */
 int main(int argc, char* argv[]) {
     Application thisApplication; //the application
 

a6a73866de341e3d3bb4b705886d0584e7fdf194
Richard sikra <richiesikra@gmail.com>
2020-02-09 22:16:52 -0800

    Fixed error with bonecollisionmesh assertion

--- src/game/components/Camera.h
+++ src/game/components/Camera.h
@@ -60,8 +60,8 @@ private:
     glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
 
-    glm::vec3 rotation = glm::vec3(90.f, 0.f, -90.f);
-    glm::vec3 targetPosition;
+    glm::vec3 rotation       = glm::vec3(90.f, 0.f, -90.f);
+    glm::vec3 targetPosition = glm::vec3(0, 0, 0);
 
     friend class CameraSystem;
 };

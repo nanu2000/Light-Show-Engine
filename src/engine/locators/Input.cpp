691265d7cc3db5ec0e7fa1df37f542dce66e5cf9
Richard sikra <richiesikra@gmail.com>
2020-02-13 03:39:44 -0800

    Cleaned code and commented

--- src/engine/locators/Input.cpp
+++ src/engine/locators/Input.cpp
@@ -1,93 +1,85 @@
 #include "Input.h"
 
-bool Input::isMousePressedOnce(MOUSE_BUTTON ind) {
-
-    int8_t index = (int8_t)ind;
-
-    bool isPressed = isMouseButtonPressed(ind);
-
-    return false;
+bool Input::isMousePressedOnce(MOUSE_BUTTON ind, float dt) {
+    return isPressedOnce(mouseData[(int8_t)ind], dt);
 }
 
 bool Input::isKeyDown(const SDL_Keycode& keycode) {
-    return keys[SDL_GetScancodeFromKey(keycode)];
+    return keyData[SDL_GetScancodeFromKey(keycode)].isPressed;
 }
 
 bool Input::isKeyPressedOnce(const SDL_Keycode& keycode, float dt) {
-    SDL_Scancode scancode              = SDL_GetScancodeFromKey(keycode);
-    PressedOnceLogic& pressedOnceLogic = keysPressedOnce[scancode];
-    bool isKeyDown                     = keys[scancode];
+    return isPressedOnce(keyData[SDL_GetScancodeFromKey(keycode)], dt);
+}
 
-    if (isKeyDown) {
-        if (pressedOnceLogic.timeUntilContinious == 0 && pressedOnceLogic.timeBetweenPress == 0) {
+Input::Input() {
+    SDL_PumpEvents();
+}
+
+void Input::handleEvents(SDL_Event& sdlEventSystem, SDL_EventType t) {
+
+    switch (t) {
+    case SDL_KEYDOWN:
+        keyData[sdlEventSystem.key.keysym.scancode].isPressed = true;
+        break;
+    case SDL_KEYUP:
+        keyData[sdlEventSystem.key.keysym.scancode].isPressed = false;
+        break;
+    case SDL_QUIT:
+        //Call to close the game.
+        GameInfo::terminateGame();
+        break;
+    case SDL_MOUSEBUTTONDOWN:
+        mouseData[sdlEventSystem.button.button - 1].isPressed = true;
+        break;
+    case SDL_MOUSEBUTTONUP:
+        mouseData[sdlEventSystem.button.button - 1].isPressed = false;
+        break;
+    default:
+        break;
+    }
+
+    SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
+}
+
+bool Input::isPressedOnce(KeyData& data, float dt) {
+
+    if (data.isPressed) {
+        if (data.timeUntilContinious == 0 && data.timeBetweenPress == 0) {
             //Set to reset value and return true
-            pressedOnceLogic.timeUntilContinious = pressedOnceLogic.timeUntilContiniousReset;
-            pressedOnceLogic.timeBetweenPress    = pressedOnceLogic.timeBetweenPressReset;
+            data.timeUntilContinious = data.timeUntilContiniousReset;
+            data.timeBetweenPress    = data.timeBetweenPressReset;
 
             //Return true on first input
             return true;
         }
 
-        pressedOnceLogic.timeUntilContinious -= dt;
-        if (pressedOnceLogic.timeUntilContinious <= 0) {
+        data.timeUntilContinious -= dt;
+        if (data.timeUntilContinious <= 0) {
             return true;
         }
 
-        pressedOnceLogic.timeBetweenPress -= dt;
-        if (pressedOnceLogic.timeBetweenPress <= 0) {
-            pressedOnceLogic.timeBetweenPress = pressedOnceLogic.timeBetweenPressReset;
+        data.timeBetweenPress -= dt;
+        if (data.timeBetweenPress <= 0) {
+            data.timeBetweenPress = data.timeBetweenPressReset;
             return true;
         }
 
     } else {
 
-        pressedOnceLogic.timeUntilContinious = 0;
-        pressedOnceLogic.timeBetweenPress    = 0;
+        data.timeUntilContinious = 0;
+        data.timeBetweenPress    = 0;
         return false;
     }
 
     return false;
 }
 
-Input::Input() {
-    SDL_PumpEvents();
-
-    //set all keys as false
-    std::fill_n(keys, SDL_NUM_SCANCODES, false);
-}
-
-void Input::handleEvents(SDL_Event& sdlEventSystem, SDL_EventType t) {
-    if (t == SDL_KEYDOWN) {
-        keys[sdlEventSystem.key.keysym.scancode] = true;
-    }
-    if (t == SDL_KEYUP) {
-        keys[sdlEventSystem.key.keysym.scancode] = false;
-    }
-
-    if (t == SDL_QUIT) {
-        /**********************|
-		|We exit the game Here |
-		|**********************/
-        GameInfo::terminateGame();
-        /*********************/
-    }
-
-    SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
-
-    if (t == SDL_MOUSEBUTTONDOWN) {
-        mouseArray[sdlEventSystem.button.button - 1] = true;
-    }
-
-    if (t == SDL_MOUSEBUTTONUP) {
-        mouseArray[sdlEventSystem.button.button - 1] = false;
-    }
-}
-
 bool Input::isMouseButtonPressed(MOUSE_BUTTON ind) {
     int8_t index = (int8_t)ind;
 
     if (index <= 2 && index >= 0) {
-        return mouseArray[index];
+        return mouseData[index].isPressed;
     } else {
         return false;
     }
@@ -109,7 +101,7 @@ void NullInput::handleEvents(SDL_Event& sdlEventSystem, SDL_EventType t) {
     }
 }
 
-bool NullInput::isMousePressedOnce(MOUSE_BUTTON index) {
+bool NullInput::isMousePressedOnce(MOUSE_BUTTON index, float dt) {
     return false;
 }
 
--- src/engine/locators/Input.h
+++ src/engine/locators/Input.h
@@ -15,44 +15,55 @@ class Input {
 public:
     Input();
 
-    virtual bool isMousePressedOnce(MOUSE_BUTTON);
-
-    virtual bool isKeyDown(const SDL_Keycode& keycode);
-
-    virtual bool isKeyPressedOnce(const SDL_Keycode& keycode, float dt);
-
+    //!Checks if the mouse is pressed and returns true if the timeBetweenPress is zero, if it's the first press, or if timeUntilContinious is 0.
+    virtual bool isMousePressedOnce(MOUSE_BUTTON, float dt);
+    //!Checks if a mouse button is pressed.
     virtual bool isMouseButtonPressed(MOUSE_BUTTON);
 
+    //!Checks if the key is pressed and returns true if the timeBetweenPress is zero, if it's the first press, or if timeUntilContinious is 0.
+    virtual bool isKeyPressedOnce(const SDL_Keycode& keycode, float dt);
+
+    //!Checks if a key is pressed.
+    virtual bool isKeyDown(const SDL_Keycode& keycode);
+
+    //!Called by Application.cpp to handle SDL's new events.
     virtual void handleEvents(SDL_Event& sdlEventSystem, SDL_EventType t);
 
+    //!Returns the mouse position relative to the top left of the screen.
     inline virtual glm::ivec2 Input::getMousePosition() const {
         return mousePosition;
     }
 
+    //!Returns the mouse position relative to the center of the screen.
+    //!To get the delta value, the mouse position is expected to be set to the window's center every FixedUpdate.
     inline virtual glm::ivec2 Input::getMouseDelta() const {
         return glm::vec2(mousePosition.x - GameInfo::getWindowWidth() / 2.f, mousePosition.y - GameInfo::getWindowHeight() / 2);
     }
 
 private:
-    struct PressedOnceLogic {
+    //!Contains data for all of the keys.
+    struct KeyData {
         float timeUntilContinious      = 0.f; //one second until press is continious.
         float timeUntilContiniousReset = 1.f;
 
-        float timeBetweenPress      = 0.f;
+        float timeBetweenPress      = 0.f; //time between returning true if pressed on isPressedOnce
         float timeBetweenPressReset = .33f;
+
+        bool isPressed = false;
+        bool isKeyUp   = false; //for future.
     };
 
-    bool keys[SDL_NUM_SCANCODES];
-    PressedOnceLogic keysPressedOnce[SDL_NUM_SCANCODES];
+    //!Checks if the KeyData d is pressed and returns true if the timeBetweenPress is zero, if it's the first press, or if timeUntilContinious is 0.
+    bool isPressedOnce(KeyData& data, float dt);
+
+    KeyData keyData[SDL_NUM_SCANCODES];
+    KeyData mouseData[3];
 
     glm::ivec2 mousePosition     = glm::ivec2(0, 0);
     glm::ivec2 mouseLastPosition = glm::ivec2(0, 0);
-
-    bool mouseArray[3] = { false, false, false };
-
-    bool pressed[3] = { false, false, false };
 };
 
+//!Used for the InputLocator if no services have been provided
 class NullInput : public Input {
 
 public:
@@ -66,7 +77,7 @@ public:
 
     bool isKeyPressedOnce(const SDL_Keycode& keycode, float dt) override;
 
-    bool isMousePressedOnce(MOUSE_BUTTON) override;
+    bool isMousePressedOnce(MOUSE_BUTTON, float dt) override;
 
     inline virtual glm::ivec2 NullInput::getMouseDelta() const override {
         return glm::ivec2(0, 0);
